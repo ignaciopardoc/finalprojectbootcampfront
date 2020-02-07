ba0300a9bfe51f8b090caac3f2766984cd8b6811
Ignacio Pardo Cardona <ignaciopardocardona@gmail.com>
2020-02-07 12:06:51 +0100

    Added filter for home map

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,6 +4,8 @@ import CardSection from "../CardSection/CardSection ";
 import "./style.css";
 import Swal from "sweetalert2";
 
+const API_CATEGORIES = "http://localhost:3000/business/getCategories";
+
 const URL_GET_MAP = "http://localhost:3000/business/getBusinessMap";
 
 interface businessDB {
@@ -30,8 +32,10 @@ interface IState {
   lonRight: number | null;
   lonLeft: number | null;
   searchInput: string;
-  latlon: number[]
-  zoom: number | null
+  latlon: number[];
+  zoom: number | null;
+  categories: [];
+  category: string;
 }
 
 class Home extends React.Component<any, IState> {
@@ -40,13 +44,15 @@ class Home extends React.Component<any, IState> {
 
     this.state = {
       businessOnMap: [],
-      latBottom: null,
-      latTop: null,
-      lonRight: null,
-      lonLeft: null,
+      latBottom: 0,
+      latTop: 0,
+      lonRight: 0,
+      lonLeft: 0,
       searchInput: "",
       latlon: [],
-      zoom: null
+      zoom: null,
+      categories: [],
+      category: ""
     };
   }
 
@@ -61,9 +67,13 @@ class Home extends React.Component<any, IState> {
         if (json.length) {
           //Result is only used for the map
           console.log(json);
-          this.setState({latlon: [json[0].lat, json[0].lon], zoom: 15})
-        }else{
-          Swal.fire({title: "No se ha encontrado ninguna dirección", text: "Inténtelo de nuevo", icon: "warning"})
+          this.setState({ latlon: [json[0].lat, json[0].lon], zoom: 15 });
+        } else {
+          Swal.fire({
+            title: "No se ha encontrado ninguna dirección",
+            text: "Inténtelo de nuevo",
+            icon: "warning"
+          });
         }
       }
     });
@@ -91,12 +101,20 @@ class Home extends React.Component<any, IState> {
   //   });
   // };
 
+  getCategories = async () => {
+    const response = await fetch(API_CATEGORIES);
+    const json = await response.json();
+    this.setState({ categories: json });
+  };
+
   getBusinessesByCoord = async (
     latBottom: number,
     latTop: number,
     lonLeft: number,
     lonRight: number
   ) => {
+    this.setState({ latBottom, latTop, lonLeft, lonRight });
+    const { category } = this.state;
     await fetch(URL_GET_MAP, {
       method: "POST",
       headers: new Headers({
@@ -106,7 +124,8 @@ class Home extends React.Component<any, IState> {
         latBottom,
         latTop,
         lonLeft,
-        lonRight
+        lonRight,
+        category
       })
     }).then(async response => {
       const json = await response.json();
@@ -114,24 +133,70 @@ class Home extends React.Component<any, IState> {
     });
   };
 
+  getBusinessesByCoordAndCategory = async () => {
+    const { latBottom, lonLeft, latTop, lonRight, category } = this.state;
+    await fetch(URL_GET_MAP, {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({
+        latBottom,
+        latTop,
+        lonLeft,
+        lonRight,
+        category
+      })
+    }).then(async response => {
+      const json = await response.json();
+      this.setState({ ...this.state, businessOnMap: json });
+    });
+  };
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
   render() {
     return (
       <Fragment>
         <div className="container">
           <h1>Descubre los mejores lugares para perros de tu ciudad</h1>
-
-          <input
-            type="text"
-            className="form-control"
-            onChange={e => this.setState({ searchInput: e.target.value })}
-            value={this.state.searchInput}
-            onKeyDown={e => {
-              if (e.keyCode === 13) {
-                this.searchByAdress();
-              }
-            }}
-            placeholder="Buscar por dirección"
-          />
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="text"
+                className="form-control"
+                onChange={e => this.setState({ searchInput: e.target.value })}
+                value={this.state.searchInput}
+                onKeyDown={e => {
+                  if (e.keyCode === 13) {
+                    this.searchByAdress();
+                  }
+                }}
+                placeholder="Buscar por dirección"
+              />
+            </div>
+            <div className="col-3">
+              <select
+                className="custom-select"
+                onChange={e => {
+                  this.setState({ category: e.target.value });
+                  setTimeout(() => {
+                    this.getBusinessesByCoordAndCategory();
+                  }, 20);
+                  
+                }}
+              >
+                <option value="null">Filtrar por categoría</option>
+                {this.state.categories.map((c, i) => (
+                  <option value={c} key={i}>
+                    {c}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
           <HomeMap
             businessOnMap={this.state.businessOnMap}
             getBusinessesByCoord={this.getBusinessesByCoord}
